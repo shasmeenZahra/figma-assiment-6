3472a1ab5890e45286b384230f3372f917739557
shasmeen zahra <165050270+shasmeenZahra@users.noreply.github.com>
2024-12-01 14:29:58 +0500

    Update last commit

--- src/app/components/Header-one.tsx
+++ src/app/components/Header-one.tsx
@@ -7,14 +7,18 @@ import { FaLinkedin } from "react-icons/fa";
 
 const headerone = () => {
   return (
+
     
-    <div className="lg:w-[1280px] lg:h-[54px] h-[72px] w-[428px] bg-[#F7F7F7] flex flex-row mb-2 border-b border-black lg:border-black lg:border-b">
-      <div className="lg:w-[1120px] lg:h-[24px] h-[42px] w-[400px] lg:flex lg:flex-row lg:justify-between lg:space-x-36 lg:items-center lg:mt-4 lg:ml-20">
-    <div className="lg:flex text-sm lg:space-x-2  mt-6 lg:flex-row ">
-      <p className="">Phone Number: 956 742 455 678</p>
-      <span className="hidden sm:inline-block ">|</span>
-      <p>Email:info@ddsgnr.com</p>
+
+ <div className="lg:w-[1280px] lg:h-[54px]  w-[428px] h-[72px] bg-[#F7F7F7] mb-2 flex flex-row border-b border-black lg:border-b lg:border-black">
+      <div className="lg:w-[1120px] lg:h-[24px] w-[400px] h-[42px] lg:flex lg:flex-row  lg:justify-between lg:space-x-36 lg:items-center lg:mt-4 lg:ml-20">
+    <div className="  flex flex-col  text-xs   lg:flex lg:flex-row lg:text-sm lg:items-center lg:space-x-2 lg:ml-2 lg:w-[386px] lg:h-[30px] w-[300px] h-[15px] ml-2 mb-7 ">
+      <div className='mt-6 lg:flex lg:flex-row lg:space-x-2'><p className="">Phone Number: 956 742 455 678</p>
+      <p className='   lg:border-l  lg:border-[#676767] lg:w-30px lg:h-[20px] '></p>
+      <p className=''>Email:info@ddsgnr.com</p>
     </div>
+</div>
+     
 
        <div className="flex flex-row justify-end lg:space-x-4 space-x-2 text-xl ml-16 lg:ml-11">
       <BiLogoFacebook />
