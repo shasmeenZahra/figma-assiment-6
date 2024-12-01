0cf02f7c2d0f8aa5febd22701ad993888ed7b615
shasmeen zahra <165050270+shasmeenZahra@users.noreply.github.com>
2024-12-01 14:40:14 +0500

    Update CoursesCard.tsx

--- src/app/components/CoursesCard.tsx
+++ src/app/components/CoursesCard.tsx
@@ -21,10 +21,10 @@ const Courses = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-12">
 
 
-        {/* Card 1 */}
+        
         <div className=" bg-white  overflow-hidden ">
           <Image
-            src="/image/image1.jpg"
+            src="/image/image1.png"
             alt="UX/UI Design 201"
             width={400}
             height={300}
